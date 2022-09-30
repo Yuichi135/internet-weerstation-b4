367604eff859bbcc8b4bb5dbcb5f59d89e7873bb
Sam-D-C <sjp.decraen@student.avans.nl>
2022-09-30 16:50:07 +0200

    code sam

---
+++ src/AAA.java
@@ -0,0 +1,7 @@
+public class AAA {
+    public static void main(String[] args) {
+        IO IO = new IO();
+
+        IO.writeShort(0x10, 2);
+    }
+}
--- src/GuiBoardDemos.java
+++ src/GuiBoardDemos.java
@@ -1,8 +1,9 @@
 public class GuiBoardDemos {
     public static void main(String[] args) {
         IO.init();
-        clear();
-        counterOn0x10();
+
+//        clear();
+//        counterOn0x10();
         clear();
         countToTenThousand();
 
@@ -11,22 +12,23 @@ public class GuiBoardDemos {
     public static void counterOn0x10() {
         for (int i = 0; i < 10; i++) {
             IO.writeShort(0x10, i);
-            IO.delay(1000);
+            IO.delay(500);
         }
     }
 
     public static void clear() {
-        IO.writeShort(0x10, 0b100000000);
-        IO.writeShort(0x12, 0b100000000);
-        IO.writeShort(0x14, 0b100000000);
-        IO.writeShort(0x16, 0b100000000);
-        IO.writeShort(0x18, 0b100000000);
-        IO.writeShort(0x20, 0b100000000);
-        IO.writeShort(0x22, 0b100000000);
-        IO.writeShort(0x24, 0b100000000);
-        IO.writeShort(0x30, 0b100000000);
-        IO.writeShort(0x32, 0b100000000);
-        IO.writeShort(0x34, 0b100000000);
+        int a = 0b100000000;
+        IO.writeShort(0x10, a);
+        IO.writeShort(0x12, a);
+        IO.writeShort(0x14, a);
+        IO.writeShort(0x16, a);
+        IO.writeShort(0x18, a);
+        IO.writeShort(0x20, a);
+        IO.writeShort(0x22, a);
+        IO.writeShort(0x24, a);
+        IO.writeShort(0x30, a);
+        IO.writeShort(0x32, a);
+        IO.writeShort(0x34, a);
     }
 
     public static void countToTenThousand() {
@@ -36,23 +38,52 @@ public class GuiBoardDemos {
         boolean thousandRunning;
         boolean tenThousandRunning;
         int ten = 0;
-        int hundred;
-        int thousand;
-        int tenThousand;
+        int hundred = 0;
+        int thousand = 0;
+        int tenThousand = 0;
 
-        while(running) {
-            for (int i = 0; i < 10; i++) {
-                IO.writeShort(0x10, i);
-                IO.delay(1000);
-
-                if (i == 9) {
-                    ten ++;
-                }
-            }
-
+        while (running) {
+         // for-loops
         }
-    }
-}
 
-
+//        while(running) {
+//            for (int i = 0; i < 10; i++) {
+//                IO.writeShort(0x10, i);
+//                IO.delay(80);
+//
+//                if (i % 10 == 0) {
+//                    IO.delay(80);
+//                    if (ten == 9){
+//                        ten =0;
+//                    } else {
+//                    ten ++;
+//                    }
+//
+//                    IO.writeShort(0x12, ten);
+//                    IO.delay(500);
+//                }
+//                    if (i == 9 && ten == 9) {
+//                        IO.delay(500);
+//                        hundred ++;
+//                        IO.writeShort(0x14, hundred);
+//                        IO.delay(500);
+//                    }
+//                        if (i == 9 && ten == 9 && hundred ==9 ) {
+//                            thousand ++;
+//                            IO.writeShort(0x16, thousand);
+//                            IO.delay(500);
+//                        }
+//                            if (thousand == 9) {
+//                                tenThousand ++;
+//                                IO.writeShort(0x18, tenThousand);
+//                                IO.delay(500);
+//                            }
+//
+//
+//
+//            }
+//
+//        }
+//    }
+//}
 
