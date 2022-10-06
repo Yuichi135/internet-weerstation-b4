fa14d7d5bca8bc9e900bf2a2371bc8aca4991d1d
Sander2482039 <sanderderegt@home.nl>
2022-10-06 10:19:23 +0200

    Nieuwe klasse

--- src/Period.java
+++ src/Period.java
@@ -1,2 +1,134 @@
+import java.time.*;
+import java.time.temporal.*;
+import java.util.ArrayList;
+
+/**
+ * A class to contain a period of time
+ *
+ * @author Johan Talboom
+ * @version 2.0
+ */
 public class Period {
+	private LocalDate beginPeriod;
+	private LocalDate endPeriod;
+
+	/**
+	 * default constructor, sets the period to today
+	 */
+	public Period() {
+		beginPeriod = LocalDate.now();
+		endPeriod = LocalDate.now();
+	}
+
+	public Period(LocalDate beginPeriod, LocalDate endPeriod) {
+		this.beginPeriod = beginPeriod;
+		this.endPeriod = endPeriod;
+	}
+
+	public Period(LocalDate beginPeriod) {
+		this.beginPeriod = beginPeriod;
+		this.endPeriod = LocalDate.now();
+	}
+
+	public Period(int days) {
+		this.beginPeriod = LocalDate.now().minus(java.time.Period.ofDays(days));
+		this.endPeriod = LocalDate.now();
+	}
+
+	/**
+	 * Simple setter for start of period
+	 */
+	public void setStart(int year, int month, int day) {
+		beginPeriod = LocalDate.of(year, month, day);
+	}
+
+	/**
+	 * simple setter for end of period
+	 */
+	public void setEnd(int year, int month, int day) {
+		endPeriod = LocalDate.of(year, month, day);
+	}
+
+	/**
+	 * alternative setter for start of period
+	 *
+	 * @param beginPeriod
+	 */
+	public void setStart(LocalDate beginPeriod) {
+		this.beginPeriod = beginPeriod;
+	}
+
+	/**
+	 * alternative setter for end of period
+	 *
+	 * @param endPeriod
+	 */
+	public void setEnd(LocalDate endPeriod) {
+		this.endPeriod = endPeriod;
+	}
+
+	/**
+	 * calculates the number of days in the period
+	 */
+	public long numberOfDays() {
+		return ChronoUnit.DAYS.between(beginPeriod, endPeriod);
+	}
+
+
+	/**
+	 * gets all raw measurements of this period from the database
+	 * @return a list of raw measurements
+	 */
+	public ArrayList<RawMeasurement> getRawMeasurements() {
+		return DatabaseConnection.getMeasurementsBetween(LocalDateTime.of(beginPeriod, LocalTime.of(0, 1)), LocalDateTime.of(endPeriod, LocalTime.of(23, 59)));
+	}
+
+	/**
+	 * Builds an ArrayList of measurements. This method also filters out any 'bad' measurements
+	 * @return a filtered list of measurements
+	 */
+	public ArrayList<Measurement> getMeasurements() {
+		ArrayList<Measurement> measurements = new ArrayList<>();
+		ArrayList<RawMeasurement> rawMeasurements = getRawMeasurements();
+		for (RawMeasurement rawMeasurement : rawMeasurements) {
+			Measurement measurement = new Measurement(rawMeasurement);
+			if(measurement.isValid()) {
+				measurements.add(measurement);
+			}
+		}
+		return measurements;
+	}
+
+
+	/**
+	 * todo
+	 * @return
+	 */
+	public double getAverageOutsideTemperature()
+	{
+		ArrayList<Measurement> measurements = getMeasurements();
+
+		//calculate average outside temperature and return it
+		return measurements.get(0).getTemperature();
+	}
+
+	/**
+	 * Todo
+	 */
+	public ArrayList<Period> hasHeatWave() {
+		return null;
+	}
+
+	/**
+	 * Todo
+	 */
+	public Period longestDraught() {
+		return new Period();
+	}
+
+	/**
+	 * Todo more methods
+	 */
+
+
 }
---
+++ src/PeriodTestMain.java
@@ -0,0 +1,11 @@
+public class PeriodTestMain {
+	public static void main(String[] args){
+
+		Period period = new Period(7); // last week
+
+		System.out.println("The average temperature of last week was " + period.getAverageOutsideTemperature());
+
+
+	}
+
+}
