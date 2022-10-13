6f1c202e7ef97fa5870d23bcccdb6a6287b36038
Rick <rrhvanhoof@gmail.com>
2022-10-13 09:59:34 +0200

    Individuele opracht

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+        boolean running = true;
+        int grootsteConsecutiveDays = 0;
+        int consecutiveDays = 0;
+        double mmGevallen = 0;
+        double totaalMmGevallen = 0;
+
+        while (running) {
+            i = klassen.getRandomNumber();
+            System.out.println(i);
+
+            if (i == 0) {
+                consecutiveDays = 0;
+                mmGevallen = 0;
+            } else {
+                mmGevallen += klassen.berekenRegen(i);
+                consecutiveDays++;
+            }
+            if (consecutiveDays > grootsteConsecutiveDays) {
+                grootsteConsecutiveDays = consecutiveDays;
+                totaalMmGevallen = mmGevallen;
+            }
+            klassen.print(grootsteConsecutiveDays, totaalMmGevallen);
+            Thread.sleep(1000);
+        }
+    }
+}
+
---
+++ src/klassen.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class klassen {
+    public static int getRandomNumber() {
+        Random r = new Random();
+        int highest = 50;
+        int lowest = 0;
+        return r.nextInt(highest-lowest) + lowest;
+    }
+
+    public static void print(int consecutiveDays, double mmGevallen) {
+        System.out.println("consecutive minutes of rain: " + consecutiveDays + " minutes");
+        System.out.println("totaal milimeter fallen: " + Math.round(mmGevallen* 1000) /1000.0 + " mm\n");
+    }
+
+    public static double berekenRegen(int rainrate) {
+        return (double) rainrate * 0.2 /60;
+    }
+}
